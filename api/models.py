08ea8ea5149df88be372afbbb390a1f2f5e7c9fb
Albert Datui <albertdatui@Alberts-MacBook-Air.local>
2015-04-04 18:47:58 +0800

    added shop in models.py

--- api/models.py
+++ api/models.py
@@ -27,6 +27,27 @@ class Order(models.Model):
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=PENDING)
     sender = models.ForeignKey(Customer, related_name="ordered")
     receiver = models.ForeignKey(Customer, related_name="received")
+    #productnya ilang
 
     def __unicode__(self):
         return self.pk
+
+
+class Shop(models.Model):
+    OPEN = 'O'
+    CLOSE = 'C'
+    TEST = 'T'
+    STATUS_CHOICES = (
+        (OPEN, 'Open'),
+        (CLOSE, "Close"),
+        (TEST, 'Test'),
+    )
+    name = models.TextField(blank=False)
+    #how to ensure that the identifier is unique?
+    identifier = models.CharField(max_length=255, blank=False, unique=True)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=CLOSE)
+    admin = models.ForeignKey(Customer, related_name="shop")
+
+    def __unicode__(self):
+        return self.name
