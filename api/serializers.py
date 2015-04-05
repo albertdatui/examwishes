772bd7c243c231c1c7f9ea8e627f87e6a72a65fc
Albert Datui <albertdatui@Alberts-MacBook-Air.local>
2015-04-05 19:55:15 +0800

    updated shopserializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,14 +6,18 @@ from api.models import *
 class OrderSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Order
-		fields = ('id', 'message', 'photo', 'status', 'sender', 'receiver')
+		fields = ('id', 'message', 'photo', 'status', 'sender', 'receiver', 'product')
 
 class ShopSerializer(serializers.ModelSerializer):
+	product = serializers.PrimaryKeyRelatedField(many=True, queryset=Shop.objects.all())
+	manager = serializers.PrimaryKeyRelatedField(many=True, queryset=Customer.objects.all())
+	admin = serializers.ReadOnlyField(source='admin.username')
 	class Meta:
 		model = Shop
-		fields = ('name', 'identifier', 'description', 'status', 'admin')
+		fields = ('name', 'identifier', 'description', 'status', 'admin', 'product', 'manager')
 
 class ProductSerializer(serializers.ModelSerializer):
+	#kalo gw gak salah inget, lu harus specify di sini buat nge-query image
 	class Meta:
 		model = Product
 		fields('id', 'name', 'description', 'videoURL', 'quantity', 'price', 'isPhotoRequired', 'shop')
